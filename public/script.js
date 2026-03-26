aef8bdbd17a4d8ea35da672cc9eec3e752fa34af
Roxy <roxyfokker@hotmail.com>
2026-03-26 16:49:48 +0100

    style: popup styling en form styling #2

--- public/script.js
+++ public/script.js
@@ -1,6 +1,12 @@
 const btnPopup = document.querySelector('.button--new');
-const popup = document.querySelector('.instrument-popup');
+const popup = document.querySelector('.instrument-toevoegen-popup');
+const btnPopup2 = document.querySelector('.popup-sluiten')
 
 btnPopup.addEventListener('click', () => {
-    popup.classList.toggle('is-open');
+    popup.classList.add('is-open');
 });
+
+btnPopup2.addEventListener('click', () => {
+    popup.classList.remove('is-open');
+});
+
